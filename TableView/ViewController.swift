96fbc76a5d18a82856df9be4f2218f7913f2e8b3
Yugandhara Lad <yugandharalad9@gmail.com>
2017-09-08 16:09:34 -0400

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Yugandhara Lad on 9/8/17.
+//  Copyright © 2017 Yugandhara Lad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

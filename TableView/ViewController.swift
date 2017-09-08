529fad4baa76cc4706b2fa778991b6abe9189872
Yugandhara Lad <yugandharalad9@gmail.com>
2017-09-08 17:29:39 -0400

    TableView Cell created

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let list = ["Milk", "Bread", "Eggs"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
